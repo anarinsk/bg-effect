00ff3f2649183b017815f49c35c55bbc8254c5b5
anarinsk <anarinsk@gmail.com>
2018-06-19 08:57:32 +0900

    @W/ Complete code_load.R. Save appannie.rds to rds direc.

--- code/code_load.R
+++ code/code_load.R
@@ -9,4 +9,24 @@ dir0 <- function(file,base=NA) {
   }
 }
 
-here::here("raw_data", "2017") %.>% list.files(.)
+### Load Excel files ----
+
+extract_filename <- function(year){
+  here::here("raw_data", str_c(year)) %.>% list.files(.)
+  }
+tibblify <- function(one_file, base){
+  one_file %.>% dir0(., base) %.>% read_excel(., skip=5, col_names=T) 
+}
+
+#UT
+#lst_excel[1] %.>% tibblify(., "raw_data/2017") 
+
+### Tibblify Excel data & save it to rds direc ----
+extract_filename(2017) %.>% map_df(., function(x){tibblify(x, "raw_data/2017")}) -> tbl1
+extract_filename(2018) %.>% map_df(., function(x){tibblify(x, "raw_data/2018")}) -> tbl2
+bind_rows(tbl1, tbl2) %.>% saveRDS(., dir0("appannie.rds", "rds")) 
+
+### End of code 
+
+
+
