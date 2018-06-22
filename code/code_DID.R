f25e4a2361174cbe805d0eef0edc28639ffb7671
anarinsk <anarinsk@gmail.com>
2018-06-22 10:40:22 +0900

    @W: code_DID working

--- code/code_DID.R
+++ code/code_DID.R
@@ -76,23 +76,54 @@ tbl0 %>%
 ### Make tbl for DID, pDID 
 ### 20180516, 20170517 - Wendnesday 
 
-tbl0 %>%
-  ungroup() %>% 
-  dplyr::filter(!date %in% c(ymd("2017-05-17", "2018-05-16"))) %>% 
-  dplyr::filter(between(rank_rv2, 1, 100)) %>% 
-  mutate(
-    year = year(date), 
-    ttm = case_when(
-      date < ymd("2017-05-17") ~ "before", 
-      date > ymd("2017-05-17") & date < ymd("2018-01-01") ~ "after", 
-      date < ymd("2018-05-16") & date > ymd("2018-01-01") ~ "before", 
-      TRUE ~ "after"
+### fun var 1: rank_rv2, rank_dl2 
+### fun var 2: rank cut 
+### fun var 3: date-cut range 
+### fun env: df 
+
+add_date <- function(x, y){ymd(x) + days(y)}
+gen_df4did <- function(var, rank, d_range, df){
+  
+  var_exp <- enquo(var)
+  date_2017 <- ymd("2017-05-17") 
+  date_2018 <- ymd("2018-05-16") 
+  
+  df %>%
+    ungroup() %>% 
+    dplyr::filter( !date %in% c(date_2017, date_2018) ) %>% 
+    dplyr::filter( between(!!var_exp, rank[[1]], rank[[2]]) ) %>% 
+    arrange(date, !!var_exp) %>% 
+    mutate(
+      year = year(date), 
+      ttm = case_when(
+        date < date_2017 ~ "before", 
+        date < date_2018 & date > ymd("2018-01-01") ~ "before", 
+        date > date_2017 & date < ymd("2018-01-01") ~ "after", 
+        TRUE ~ "after"
+      ) 
+    ) %>% 
+    dplyr::filter( 
+      between(date, add_date(date_2017, -d_range), add_date(date_2017, d_range)) |
+      between(date, add_date(date_2018, -d_range), add_date(date_2018, d_range))
+    ) %>% 
+    select(date, !!var_exp, app_name, everything()) %>% 
+    mutate(
+      fct_year = factor(year), 
+      fct_ttm = factor(ttm), 
+      fct_franchise = if_else(is.na(app_franchise), FALSE, TRUE), 
+      fct_domestic = if_else(hq_country == "South Korea", TRUE, FALSE), 
+      age = date - app_release_date
     )
-  ) %>% 
-  count(year, ttm)
+}
 
+### Unit test 
+gen_df4did(rank_dl2, c(1,100), 7, tbl0) -> df_reg
 
+### Regression Test ----
 
-
-
+gen_df4did(rank_rv2, c(1,30), 14, tbl0) %>% 
+  group_by(rank_rv2, fct_year, fct_ttm) %>% 
+  summarise(revenue = sum(revenue), download = sum(download)) %>% 
+  lm(revenue ~ download + age + star_rating +fct_domestic + fct_franchise + fct_year*fct_ttm, data =.) %>% 
+  summary()
 
